31ba724ffe20c2da75a32030d67bb2353e0894d3
saurabhjagdhane <saurabhjagdhane@gmail.com>
2016-12-03 01:34:27 -0700

    Sobel edge commented

--- artcelerationlibrary/src/main/jni/edu_asu_msrs_artcelerationlibrary_SobelEdgeFilter.cpp
+++ artcelerationlibrary/src/main/jni/edu_asu_msrs_artcelerationlibrary_SobelEdgeFilter.cpp
@@ -1,3 +1,8 @@
+/**
+ * This is native(C++) code for Sobel Edge filter image transform.
+ * This filter highlights edges in an image.
+ * It has two filters: horizontal and vertical.
+ */
 #include <edu_asu_msrs_artcelerationlibrary_SobelEdgeFilter.h>
 #include <time.h>
 #include <android/log.h>
@@ -13,7 +18,14 @@
 #define  LOGD(...)  __android_log_print(ANDROID_LOG_DEBUG,LOG_TAG,__VA_ARGS__)
 
 using namespace std;
-
+/**
+ * This function performs the Sobel edge filter operation operation.
+ * @param info : info is a pointer to AndroidBitmapInfo which provides height and width of the bitmap.
+ * @param input : Pointer to input Bitmap.
+ * @param gray : Pointer to gray Bitmap.
+ * @param output : Pointer to resultant output transformed bitmap.
+ * @param a0 : integer parameter as as intArgs[0] to select type of gradient.
+ */
 static void process(AndroidBitmapInfo* info, void* input, void* gray, void* output,int a0){
             uint32_t* line;
             uint32_t* line1;
@@ -29,21 +41,21 @@ static void process(AndroidBitmapInfo* info, void* input, void* gray, void* outp
             int sy[3][3] = {{-1,-2,-1}, {0,0,0}, {1,2,1}} ;
 
 
-            // Image represented by 4-bytes (4 channels as A,R, G, B)
+            // Image represented by 4-bytes (4 channels as A=255,R, G, B)
             int r, g, b;
             int pix;
             int i, j, k;
             int w = info->width;
             int h = info->height;
 
-            // Green
+            // Dynamic allocation for utilizing heap.
             int** gg = new int*[info->width];
             //int gg[w][h];
             for (i=0;i<info->width;i++){
                     gg[i]=new int [info->height];
             }
 
-            /* ARM NEON SIMD implementation
+            /* ARM NEON SIMD implementation (for grayscale image)
 
                         uint8x8_t ascale = vdup_n_u8(255);
                         uint8x8_t rscale = vdup_n_u8(77);
@@ -67,8 +79,6 @@ static void process(AndroidBitmapInfo* info, void* input, void* gray, void* outp
                                 vst1_u8(line, res);
                                 line += 8;
                                 line1 += 8*4;
-
-                                //grayScale.setPixel(i,j,Color.argb(255,r,g,b));
                             }
                             pxg = (char*) pxg + info->stride;
                             pxi = (char*) pxi + info->stride;
@@ -76,67 +86,76 @@ static void process(AndroidBitmapInfo* info, void* input, void* gray, void* outp
 
             */
 
+            // Creating grayscale image.
             int value;
             for(j=0; j<h; j++){
                 line = (uint32_t*)pxg;
                 line1 = (uint32_t*)pxi;
                         for(i=0; i< w; i++){
-                            //pix=input.getPixel(i,j); // getPixel returns an integer value of the color of pixel
-
-                            // Filtering for every channel a,r,g,b
                             r = ((int)((line1[i] & 0x00FF0000)* 0.2989) >> 16);
                             g = ((int)((line1[i] & 0x0000FF00)* 0.5870) >> 8);
                             b = (int)((line1[i] & 0x00000FF)* 0.1140);
                             value = r+g+b;
+
+                            // Setting output pixel
                             line[i] =
                             (((value << 16) & 0x00FF0000) |
                             ((value << 8) & 0x0000FF00) |
                             (value & 0x000000FF)|
                             0xFF000000);
+
+                            // R=G=B. Hence, storing only 1 value for gradient calculation.
                             gg[i][j] = (line1[i] & 0x0000FF00) >> 8;
                         }
-                        pxg = (char*) pxg + info->stride;
-                        pxi = (char*) pxi + info->stride;
+                        pxg = (char*) pxg + info->stride; // Moving to next row of gray bitmap after processing current row.
+                        pxi = (char*) pxi + info->stride; // Moving to next row of input bitmap after processing current row.
                     }
 
 
 
-
+            // Output image channel contains Grx.
             if (a0==0){
                 int Grx[w][h];
                 for (j = 1; j < h-1; j++) {
                     line = (uint32_t*)pxo;
                     for (i=1;i<w-1;i++) {
-
+                        // Gradient calculation.
                         Grx[i][j] = sx[0][0]*gg[i-1][j-1] + sx[1][0]*gg[i][j-1]+ sx[2][0]*gg[i+1][j-1]
                                 + sx[0][1]*gg[i-1][j]+ sx[1][1]*gg[i][j]+ sx[2][1]*gg[i+1][j]
                                 + sx[0][2]*gg[i-1][j+1] + sx[1][2]*gg[i][j+1]+ sx[2][2]*gg[i+1][j+1];
 
+                        // Setting output pixel
                         line[i] =
                         (((Grx[i][j] << 16) & 0x00FF0000) |
                         ((Grx[i][j] << 8) & 0x0000FF00) |
                         (Grx[i][j] & 0x000000FF)|
                          0xFF000000);
                     }
-                    pxo = (char*) pxo + info->stride;
+                    pxo = (char*) pxo + info->stride; // Moving to next row of output bitmap after processing current row.
                 }
+
+                // Output image channel contains Gry.
             }else if(a0==1){
                 int Gry[w][h];
                 for (j = 1; j < h-1; j++) {
                         line = (uint32_t*)pxo;
                         for (i=1;i<w-1;i++) {
+
+                            // Gradient calculation.
                         Gry[i][j] =sy[0][0]*gg[i-1][j-1] + sy[1][0]*gg[i][j-1]+ sy[2][0]*gg[i+1][j-1]
                                 + sy[0][1]*gg[i-1][j]+ sy[1][1]*gg[i][j]+ sy[2][1]*gg[i+1][j]
                                 + sy[0][2]*gg[i-1][j+1] + sy[1][2]*gg[i][j+1]+ sy[2][2]*gg[i+1][j+1];
 
+                            // Setting output pixel
                         line[i] =
                         (((Gry[i][j] << 16) & 0x00FF0000) |
                         ((Gry[i][j] << 8) & 0x0000FF00) |
                         (Gry[i][j] & 0x000000FF)|
                         0xFF000000);
                         }
-                        pxo = (char*) pxo + info->stride;
+                        pxo = (char*) pxo + info->stride; // Moving to next row of output bitmap after processing current row.
                 }
+                // Output image channel contains overall gradient magnitude.
             }else if (a0 == 2){
                 int Grx[w][h];
                 int Gry[w][h];
@@ -144,33 +163,43 @@ static void process(AndroidBitmapInfo* info, void* input, void* gray, void* outp
                         line = (uint32_t*)pxo;
                         for (i=1;i<w-1;i++) {
 
+                            // Gradient calculation. (Grx)
                         Grx[i][j] = sx[0][0]*gg[i-1][j-1] + sx[1][0]*gg[i][j-1]+ sx[2][0]*gg[i+1][j-1]
                                 + sx[0][1]*gg[i-1][j]+ sx[1][1]*gg[i][j]+ sx[2][1]*gg[i+1][j]
                                 + sx[0][2]*gg[i-1][j+1] + sx[1][2]*gg[i][j+1]+ sx[2][2]*gg[i+1][j+1];
 
+                            // Gradient calculation. (Gry)
                         Gry[i][j] =sy[0][0]*gg[i-1][j-1] + sy[1][0]*gg[i][j-1]+ sy[2][0]*gg[i+1][j-1]
                                 + sy[0][1]*gg[i-1][j]+ sy[1][1]*gg[i][j]+ sy[2][1]*gg[i+1][j]
                                 + sy[0][2]*gg[i-1][j+1] + sy[1][2]*gg[i][j+1]+ sy[2][2]*gg[i+1][j+1];
 
+                            // overall gradient
                         int Gr = (int) sqrt(Grx[i][j]*Grx[i][j] + Gry[i][j]*Gry[i][j]);
 
+                            // Setting output pixel
                         line[i] =
                         (((Gr << 16) & 0x00FF0000) |
                         ((Gr << 8) & 0x0000FF00) |
                         (Gr & 0x000000FF)|
                         0xFF000000);
                     }
-                    pxo = (char*) pxo + info->stride;
+                    pxo = (char*) pxo + info->stride; // Moving to next row of output bitmap after processing current row.
                 }
             }
 
             for (i=0;i<info->width;i++){
-                    delete [] gg[i];
+                    delete [] gg[i];  // Deleting dynamically allocated memory.
             }
             delete [] gg;
 }
 
-
+/*
+ * JNI call for Sobel edge filter.
+ * @param a0: integer parameter as as intArgs[0] to select type of gradient.
+ * @param input: native equivalent of input bitmap.
+ * @param grayscale: native equivalent of gray bitmap.
+ * @param output: native equivalent of output bitmap.
+ */
 
 JNIEXPORT void JNICALL Java_edu_asu_msrs_artcelerationlibrary_SobelEdgeFilter_getSobelEdgeFilter
   (JNIEnv* env, jclass jc, jint a0, jobject input, jobject grayscale, jobject output){
@@ -180,26 +209,31 @@ JNIEXPORT void JNICALL Java_edu_asu_msrs_artcelerationlibrary_SobelEdgeFilter_ge
           void* pixels_output;
           void* pixels_gray;
 
-          // LOGD("a0=%d, a1=%d, size =%d\n", a0, a1, size);
+            // Get info for input bitmap.
           if ((ret = AndroidBitmap_getInfo(env, input, &info_input)) < 0) {
                   LOGE("AndroidBitmap_getInfo() failed ! error=%d", ret);
                   return;
               }
 
+            // Acquire a lock for input pixels.
           if ((ret = AndroidBitmap_lockPixels(env, input, &pixels_input)) < 0) {
               LOGE("AndroidBitmap_lockPixels() failed ! error=%d", ret);
           }
 
+            // Acquire a lock for grayscale pixels.
            if ((ret = AndroidBitmap_lockPixels(env, grayscale, &pixels_gray)) < 0) {
                         LOGE("AndroidBitmap_lockPixels() failed ! error=%d", ret);
                     }
 
+            // Acquire a lock for output pixels.
             if ((ret = AndroidBitmap_lockPixels(env, output, &pixels_output)) < 0) {
                 LOGE("AndroidBitmap_lockPixels() failed ! error=%d", ret);
             }
 
+            // Call process function for transform.
           process(&info_input, pixels_input, pixels_gray, pixels_output, a0);
 
+            // Unlock pixels.
           AndroidBitmap_unlockPixels(env, input);
           AndroidBitmap_unlockPixels(env, output);
           AndroidBitmap_unlockPixels(env, grayscale);
